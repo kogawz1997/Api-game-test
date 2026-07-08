c02a6e822b5bdcb1c04a1d14967613a012bf3bc9
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 01:05:39 +0700

    Register advanced game service

--- src/mock-provider/mock-provider.module.ts
+++ src/mock-provider/mock-provider.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProviderClientFactory } from '../provider-clients/provider-client.factory';
+import { AdvancedGameService } from './advanced-game.service';
 import { MockProviderController } from './mock-provider.controller';
 import { MockProviderService } from './mock-provider.service';
 import { MockProviderHmacGuard } from './security/mock-provider-hmac.guard';
 
 @Module({
   controllers: [MockProviderController],
-  providers: [MockProviderService, MockProviderHmacGuard, ProviderClientFactory],
+  providers: [MockProviderService, AdvancedGameService, MockProviderHmacGuard, ProviderClientFactory],
 })
 export class MockProviderModule {}

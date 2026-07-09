29719634883357b3e28af3bab8e4e5278838f3f8
KOGA.EXE <tawepornchok@gmail.com>
2026-07-09 07:57:45 +0700

    Register static UI pages controller

--- src/mock-provider/mock-provider.module.ts
+++ src/mock-provider/mock-provider.module.ts
@@ -4,9 +4,10 @@ import { AdvancedGameService } from './advanced-game.service';
 import { MockProviderController } from './mock-provider.controller';
 import { MockProviderService } from './mock-provider.service';
 import { MockProviderHmacGuard } from './security/mock-provider-hmac.guard';
+import { StaticPagesController } from './static-pages.controller';
 
 @Module({
-  controllers: [MockProviderController],
+  controllers: [MockProviderController, StaticPagesController],
   providers: [MockProviderService, AdvancedGameService, MockProviderHmacGuard, ProviderClientFactory],
 })
 export class MockProviderModule {}
